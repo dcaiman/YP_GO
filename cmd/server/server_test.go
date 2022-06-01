45928711ad181842d1b6e0acb1a439a4959bbf11
dcaiman <77855475+dcaiman@users.noreply.github.com>
2022-06-01 11:53:21 +0300

    try_03

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -9,11 +9,11 @@ import (
 func Test_updGauge(t *testing.T) {
 	var expected float64 = 5
 	updGauge("test", expected)
-	assert.Equal(t, Gauges.m["test"], expected)
+	assert.Equal(t, Metrics.Gauges["test"], expected)
 }
 
 func Test_updCounter(t *testing.T) {
 	var expected int64 = 5
 	updCounter("new", expected)
-	assert.Equal(t, Counters.m["new"], expected)
+	assert.Equal(t, Metrics.Counters["new"], expected)
 }
