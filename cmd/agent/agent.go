f9a886c876ecbf4c535094b668113552c3de1c2c
dcaiman <77855475+dcaiman@users.noreply.github.com>
2022-06-01 16:45:26 +0300

    try_06

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,35 +1,147 @@
-package agent
+package main
 
 import (
 	"fmt"
+	"math/rand"
+	"net/http"
 	"os"
 	"os/signal"
+	"reflect"
+	"runtime"
+	"strconv"
+	"sync"
 	"syscall"
 	"time"
 )
 
-var pollInterval = 2 * time.Second
-var reportInterval = 10 * time.Second
-var contentType = "text/plain"
-var srvAddr = "http://127.0.0.1:8080"
+var runtimeGauges = [...]string{
+	"Alloc",
+	"BuckHashSys",
+	"Frees",
+	"GCCPUFraction",
+	"GCSys",
+	"HeapAlloc",
+	"HeapIdle",
+	"HeapInuse",
+	"HeapObjects",
+	"HeapReleased",
+	"HeapSys",
+	"LastGC",
+	"Lookups",
+	"MCacheInuse",
+	"MCacheSys",
+	"MSpanInuse",
+	"MSpanSys",
+	"Mallocs",
+	"NextGC",
+	"NumForcedGC",
+	"NumGC",
+	"OtherSys",
+	"PauseTotalNs",
+	"StackInuse",
+	"StackSys",
+	"Sys",
+	"TotalAlloc",
+}
 
-func RunAgent() {
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+var customGauges = [...]string{
+	"RandomValue",
+}
 
-	pollTimer := time.NewTicker(pollInterval)
-	reportTimer := time.NewTicker(reportInterval)
-	for {
-		select {
-		case <-pollTimer.C:
-			poll()
-		case <-reportTimer.C:
-			report()
-		case <-signalCh:
-			fmt.Println("EXIT")
-			os.Exit(1)
-		}
+var counters = [...]string{
+	"PollCounter",
+}
+
+var metrics = struct {
+	sync.RWMutex
+	gauges   map[string]float64
+	counters map[string]int64
+}{
+	gauges:   map[string]float64{},
+	counters: map[string]int64{},
+}
+
+func getGauge(name string) (float64, error) {
+	metrics.RLock()
+	defer metrics.RUnlock()
+	if val, ok := metrics.gauges[name]; ok {
+		return val, nil
 	}
+	return 0, fmt.Errorf("cannot get: no such gauge <%v>", name)
+}
+
+func getCounter(name string) (int64, error) {
+	metrics.RLock()
+	defer metrics.RUnlock()
+	if val, ok := metrics.counters[name]; ok {
+		return val, nil
+	}
+	return 0, fmt.Errorf("cannot get: no such counter <%v>", name)
+}
+
+func updGaugeByRuntimeValue(name string) bool {
+	metrics.Lock()
+	defer metrics.Unlock()
+	m := &runtime.MemStats{}
+	runtime.ReadMemStats(m)
+	tmp := reflect.Indirect(reflect.ValueOf(m)).FieldByName(name).Convert(reflect.TypeOf(metrics.gauges[name])).Float()
+	if metrics.gauges[name] == tmp {
+		return false
+	}
+	metrics.gauges[name] = tmp
+	return true
+}
+
+func updGaugeByRandomValue(name string) {
+	metrics.Lock()
+	defer metrics.Unlock()
+	metrics.gauges[name] = 100 * rand.Float64()
+}
+
+func updCounter(name string) {
+	metrics.Lock()
+	defer metrics.Unlock()
+	metrics.counters[name] += 1
+}
+
+func resetCounter(name string) {
+	metrics.Lock()
+	defer metrics.Unlock()
+	metrics.counters[name] = 0
+}
+
+func sendGauge(srvAddr, contentType, metricName string) error {
+	val, err := getGauge(metricName)
+	if err != nil {
+		return err
+	}
+	err = postReq(srvAddr+"/update/gauge/"+metricName+"/"+strconv.FormatFloat(val, 'f', 3, 64), contentType)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func sendCounter(srvAddr, contentType, metricName string) error {
+	val, err := getCounter(metricName)
+	if err != nil {
+		return err
+	}
+	err = postReq(srvAddr+"/update/counter/"+metricName+"/"+strconv.FormatInt(val, 10), contentType)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func postReq(addr, cont string) error {
+	res, err := http.Post(addr, cont, nil)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	defer res.Body.Close()
+	fmt.Println(res.Status, res.Request.URL)
+	return nil
 }
 
 func poll() {
@@ -49,5 +161,29 @@ func report() {
 	go sendGauge(srvAddr, contentType, customGauges[0])
 	for i := range runtimeGauges {
 		go sendGauge(srvAddr, contentType, runtimeGauges[i])
+	}
+}
+
+var pollInterval = 2 * time.Second
+var reportInterval = 10 * time.Second
+var contentType = "text/plain"
+var srvAddr = "http://127.0.0.1:8080"
+
+func main() {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	pollTimer := time.NewTicker(pollInterval)
+	reportTimer := time.NewTicker(reportInterval)
+	for {
+		select {
+		case <-pollTimer.C:
+			poll()
+		case <-reportTimer.C:
+			report()
+		case <-signalCh:
+			fmt.Println("EXIT")
+			os.Exit(1)
+		}
 	}
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,14 +1,142 @@
-package server
+package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 )
 
+var Metrics = struct {
+	sync.RWMutex
+	Gauges   map[string]float64
+	Counters map[string]int64
+}{
+	Gauges:   map[string]float64{},
+	Counters: map[string]int64{},
+}
+
+func updGauge(name string, val float64) {
+	Metrics.Lock()
+	defer Metrics.Unlock()
+	Metrics.Gauges[name] = val
+}
+
+func updCounter(name string, val int64) {
+	Metrics.Lock()
+	defer Metrics.Unlock()
+	Metrics.Counters[name] += val
+}
+
+func getGauges() []string {
+	arr := []string{}
+	Metrics.RLock()
+	defer Metrics.RUnlock()
+	for k, v := range Metrics.Gauges {
+		arr = append(arr, k+": "+strconv.FormatFloat(v, 'f', 3, 64))
+	}
+	return arr
+}
+
+func getCounters() []string {
+	arr := []string{}
+	Metrics.RLock()
+	defer Metrics.RUnlock()
+	for k, v := range Metrics.Counters {
+		arr = append(arr, k+": "+strconv.FormatInt(v, 10))
+	}
+	return arr
+}
+
+func getGauge(name string) (float64, error) {
+	Metrics.RLock()
+	defer Metrics.RUnlock()
+	if val, ok := Metrics.Gauges[name]; ok {
+		return val, nil
+	}
+	return 0, fmt.Errorf("cannot get: no such gauge <%v>", name)
+}
+
+func getCounter(name string) (int64, error) {
+	Metrics.RLock()
+	defer Metrics.RUnlock()
+	if val, ok := Metrics.Counters[name]; ok {
+		return val, nil
+	}
+	return 0, fmt.Errorf("cannot get: no such counter <%v>", name)
+}
+
+func hdlrGetAll(ww http.ResponseWriter, rr *http.Request) {
+	ww.Write([]byte("METRICS LIST\n\n" + strings.Join(getGauges(), "\n") + "\n\n" + strings.Join(getCounters(), "\n")))
+}
+
+func hdlrUpdate(ww http.ResponseWriter, rr *http.Request) {
+	metricType := chi.URLParam(rr, "type")
+	metricName := chi.URLParam(rr, "name")
+	metricVal := chi.URLParam(rr, "val")
+	switch metricType {
+	case "gauge":
+		val, err := strconv.ParseFloat(metricVal, 64)
+		if err != nil {
+			http.Error(ww, err.Error(), http.StatusBadRequest)
+			return
+		}
+		updGauge(metricName, val)
+	case "counter":
+		val, err := strconv.ParseInt(metricVal, 10, 64)
+		if err != nil {
+			http.Error(ww, err.Error(), http.StatusBadRequest)
+		}
+		updCounter(metricName, val)
+	default:
+		http.Error(ww, "cannot update: no such metric type <"+metricType+">", http.StatusNotImplemented)
+		return
+	}
+}
+
+func hdlrGetMetric(ww http.ResponseWriter, rr *http.Request) {
+	metricType := chi.URLParam(rr, "type")
+	metricName := chi.URLParam(rr, "name")
+	switch metricType {
+	case "gauge":
+		if metricVal, err := getGauge(metricName); err == nil {
+			ww.Write([]byte(strconv.FormatFloat(metricVal, 'f', 3, 64)))
+		} else {
+			http.Error(ww, err.Error(), http.StatusNotFound)
+			return
+		}
+	case "counter":
+		if metricVal, err := getCounter(metricName); err == nil {
+			ww.Write([]byte(strconv.FormatInt(metricVal, 10)))
+		} else {
+			http.Error(ww, err.Error(), http.StatusNotFound)
+			return
+		}
+	default:
+		http.Error(ww, "cannot get: no such metrics type <"+metricType+">", http.StatusNotImplemented)
+		return
+	}
+}
+
+func hdlrGetMetricsByType(ww http.ResponseWriter, rr *http.Request) {
+	metricType := chi.URLParam(rr, "type")
+	switch metricType {
+	case "gauge":
+		ww.Write([]byte("GAUGES LIST:\n\n" + strings.Join(getGauges(), "\n")))
+	case "counter":
+		ww.Write([]byte("COUNTERS LIST:\n\n" + strings.Join(getCounters(), "\n")))
+	default:
+		http.Error(ww, "cannot get: no such metrics type <"+metricType+">", http.StatusNotFound)
+		return
+	}
+}
+
 var srvAddr = "127.0.0.1:8080"
 
-func RunServer() {
+func main() {
 	mainRouter := chi.NewRouter()
 	mainRouter.Route("/", func(r chi.Router) {
 		r.Get("/", hdlrGetAll)
