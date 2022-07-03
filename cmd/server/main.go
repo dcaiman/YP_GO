577e6172929a3078c5dcc42a1984ca4bdaaba290
gruzdev.ds <gruzdev.ds@inbox.ru>
2022-07-03 23:33:51 +0300

    newtry_22

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,9 +10,9 @@ func main() {
 	srv := server.ServerConfig{
 		Cfg: server.EnvConfig{
 			SrvAddr:       "127.0.0.1:8080",
-			DBAddr:        "postgresql://postgres:1@127.0.0.1:5432",
+			DBAddr:        "", //"postgresql://postgres:1@127.0.0.1:5432",
 			StoreInterval: 2 * time.Second,
-			StoreFile:     "./tmp/metricStorage.json",
+			StoreFile:     "", //"./tmp/metricStorage.json",
 			HashKey:       "key",
 			InitDownload:  true,
 
